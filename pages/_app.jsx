ad73aa194c4823fb543972e2c52b15207df2717b
Divyanshu Saini <112510115+divyanshsaini962@users.noreply.github.com>
2024-01-18 00:52:18 +0530

    add a google verification code

--- pages/_app.jsx
+++ pages/_app.jsx
@@ -1,10 +1,15 @@
 import '../styles/globals.css';
 // import type { AppProps } from 'next/app';
 import {SessionProvider} from 'next-auth/react';
+import Head from "next/head";
 
 function MyApp({ Component, pageProps:{session,...pageProps} }) {
   return (
+    
   <SessionProvider session={session}>
+    <Head>
+     <meta name="google-site-verification" content="google758691b40a024825.html" />
+    </Head>
       <Component {...pageProps} />
   </SessionProvider>
   );
