3f0f18fe258389191c8e38751b1d5456540f2647
Divyanshu Saini <112510115+divyanshsaini962@users.noreply.github.com>
2024-01-19 00:53:42 +0530

    add a tag mangar

--- pages/_app.jsx
+++ pages/_app.jsx
@@ -4,6 +4,7 @@ import {SessionProvider} from 'next-auth/react';
 import Head from "next/head";
 
 function MyApp({ Component, pageProps:{session,...pageProps} }) {
+  let GTM_ID = process.env.NEXT_PUBLIC_GTM_ID;
   return (
     
   <SessionProvider session={session}>
@@ -11,7 +12,11 @@ function MyApp({ Component, pageProps:{session,...pageProps} }) {
      <meta name="google-site-verification" content="google758691b40a024825.html" />
     </Head>
       <Component {...pageProps} />
+      <noscript>
+  <iframe src={`https://www.googletagmanager.com/ns.html?id=${GTM_ID}`} height="0" width="0" ></iframe>
+   </noscript>
   </SessionProvider>
+  
   );
 }
 
--- pages/_document.jsx
+++ pages/_document.jsx
@@ -1,19 +1,34 @@
 import { Html, Head, Main, NextScript } from "next/document";
 
+
 export default function Document() {
+  let GTM_ID = process.env.NEXT_PUBLIC_GTM_ID;
+ 
   return (
     <Html>
+
       <Head>
       <meta name="google-site-verification" content="google758691b40a024825.html" />
+      <script 
+          async
+          src={`https://www.googletagmanager.com/gtag/js?id=${GTM_ID}`} />
+
+           {/* GTM Code Starts */} 
+           <script
+             dangerouslySetInnerHTML={{
+               __html: `(function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
+           new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
+           j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
+           'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
+           })(window,document,'script','dataLayer','${GTM_ID}');`,
+             }}
+           />
+           {/* GTM Code Ends */}
         <link
           href="https://fonts.googleapis.com/css2?family=Montserrat:wght@100;200;300;400;500;600;700;800;900&family=Poppins:wght@100;200;300;400;500;600;700;800;900&display=swap"
           rel="stylesheet"
         ></link>
-        {/* <!-- Global site tag (gtag.js) - Google Analytics --> */}
-          <script
-            async
-            src={`https://www.googletagmanager.com/gtag/js?id=GTM-59WF53QL`}
-          />
+           
           <script
             dangerouslySetInnerHTML={{
               __html: `
