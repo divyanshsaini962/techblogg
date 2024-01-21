b6afc0800fc0266aabf508e5eeff13978aee5b9d
Divyanshu Saini <112510115+divyanshsaini962@users.noreply.github.com>
2024-01-21 10:16:18 +0530

    commit  google tag code

--- pages/_document.jsx
+++ pages/_document.jsx
@@ -1,5 +1,5 @@
 import { Html, Head, Main, NextScript } from "next/document";
-
+import Script from 'next/script';
 
 export default function Document() {
   let GTM_ID = process.env.NEXT_PUBLIC_GTM_ID;
@@ -9,20 +9,21 @@ export default function Document() {
 
       <Head>
       <meta name="google-site-verification" content="google758691b40a024825.html" />
-      <script 
-          async
-          src={`https://www.googletagmanager.com/gtag/js?id=${GTM_ID}`} />
+      <Script
+        src={`https://www.googletagmanager.com/gtm.js?id=GTM-599T76ZG`}
+        strategy="afterInteractive"
+      />
 
            {/* GTM Code Starts */} 
            <script
-             dangerouslySetInnerHTML={{
-               __html: `(function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
-               new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
-               j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
-               'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
-               })(window,document,'script','dataLayer','GTM-599T76ZG');`,
-             }}
-           />
+            dangerouslySetInnerHTML={{
+              __html: `(function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
+              new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
+              j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
+              'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
+              })(window,document,'script','dataLayer','GTM-599T76ZG');`,
+            }}
+          />
            {/* GTM Code Ends */}
         <link
           href="https://fonts.googleapis.com/css2?family=Montserrat:wght@100;200;300;400;500;600;700;800;900&family=Poppins:wght@100;200;300;400;500;600;700;800;900&display=swap"
@@ -34,8 +35,14 @@ export default function Document() {
       <body>
         <Main />
         <NextScript />
-        <iframe src="https://www.googletagmanager.com/ns.html?id=GTM-599T76ZG"
-         height="0" width="0"></iframe>
+         <noscript>
+            <iframe
+              src={`https://www.googletagmanager.com/ns.html?id=GTM-599T76ZG`}
+              height="0"
+              width="0"
+              style={{ display: 'none', visibility: 'hidden' }}
+            />
+          </noscript>
       </body>
     </Html>
   );
