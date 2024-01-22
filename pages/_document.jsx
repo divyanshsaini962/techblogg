f69dec0bf880cbc1b4de04b355e49c8daae3776b
Divyanshu Saini <112510115+divyanshsaini962@users.noreply.github.com>
2024-01-22 22:57:55 +0530

    google tag manager code change

--- pages/_document.jsx
+++ pages/_document.jsx
@@ -9,19 +9,22 @@ export default function Document() {
 
       <Head>
       <meta name="google-site-verification" content="google758691b40a024825.html" />
-      <Script
-        src={`https://www.googletagmanager.com/gtm.js?id=GTM-599T76ZG`}
-        strategy="afterInteractive"
-      />
+      <script
+            async
+            src={`https://www.googletagmanager.com/gtag/js?id=${GTM_ID}`}
+          />
 
            {/* GTM Code Starts */} 
            <script
             dangerouslySetInnerHTML={{
-              __html: `(function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
-              new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
-              j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
-              'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
-              })(window,document,'script','dataLayer','GTM-599T76ZG');`,
+              __html: `
+              window.dataLayer = window.dataLayer || [];
+              function gtag(){dataLayer.push(arguments);}
+              gtag('js', new Date());
+              gtag('config', '${GTM_ID}', {
+                page_path: window.location.pathname,
+              });
+            `,
             }}
           />
            {/* GTM Code Ends */}
@@ -35,14 +38,6 @@ export default function Document() {
       <body>
         <Main />
         <NextScript />
-         <noscript>
-            <iframe
-              src={`https://www.googletagmanager.com/ns.html?id=GTM-599T76ZG`}
-              height="0"
-              width="0"
-              style={{ display: 'none', visibility: 'hidden' }}
-            />
-          </noscript>
       </body>
     </Html>
   );
