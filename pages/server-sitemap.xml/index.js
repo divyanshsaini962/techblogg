8ca511dba29340042ffbb6cb3c566e0e2707694c
Divyanshu Saini <112510115+divyanshsaini962@users.noreply.github.com>
2024-01-28 23:53:35 +0530

    comment a sitemap

--- pages/server-sitemap.xml/index.js
+++ pages/server-sitemap.xml/index.js
@@ -1,18 +1,18 @@
-import Axios from 'axios';
-import { getServerSideSitemap } from 'next-sitemap'; 
-import { getServerSideProps } from 'next-sitemap';
+// import Axios from 'axios';
+// import { getServerSideSitemap } from 'next-sitemap'; 
+// import { getServerSideProps } from 'next-sitemap';
 
-export async function getServerSideProps(context) {
-    const response = await Axios.get(
-        `base_url/<data>`,
-    );
-    const data = await response?.data;
-    const fields = data?.map((item)=>({
-        loc:`https://www.techblogservice.online/${item?.url}`,
-        lastmodi:new Date().toISOString()
-    }))
-    return getServerSideSitemap(context,fields);
+// export async function getServerSideProps(context) {
+//     const response = await Axios.get(
+//         `base_url/<data>`,
+//     );
+//     const data = await response?.data;
+//     const fields = data?.map((item)=>({
+//         loc:`https://www.techblogservice.online/${item?.url}`,
+//         lastmodi:new Date().toISOString()
+//     }))
+//     return getServerSideSitemap(context,fields);
 
-}
+// }
 
-export default function Site(){}
+// export default function Site(){}
