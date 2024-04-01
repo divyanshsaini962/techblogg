5c4099041e7c69ec20a26158239e7d13d405230e
Divyanshu Saini <112510115+divyanshsaini962@users.noreply.github.com>
2024-04-02 01:51:54 +0530

    next config file change

--- next.config.js
+++ next.config.js
@@ -1,7 +1,20 @@
 /** @type {import('next').NextConfig} */
 module.exports = {
   reactStrictMode: true,
-  images:{
-    domains:["cdn.sanity.io"],
+  images: {
+    domains: ["cdn.sanity.io"],
+  },
+  webpack: (config, { isServer }) => {
+    // Add XML loader
+    config.module.rules.push({
+      test: /\.xml$/,
+      use: [
+        {
+          loader: 'xml-loader',
+        },
+      ],
+    });
+
+    return config;
   },
 };
